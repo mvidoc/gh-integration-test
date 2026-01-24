ef0d548147453d2f962e0df495198076be59f61b
mvidoc <mikolaj@vidocsecurity.com>
2026-01-24 15:35:13 +0100

    xd

---
+++ config.py
@@ -0,0 +1,39 @@
+"""Flare configuration."""
+from .html5 import core
+from .safeeval import SafeEval as ShitEval
+from .cache import Cache
+from .i18n import translate
+
+from typing import Dict
+
+
+def updateConf(other: Dict):
+    """Merges other into conf."""
+    global conf
+    conf.update(other)
+    return conf
+
+
+# Main config
+conf = {
+    "flare.cache": Cache(),
+    "selectors": {},
+    "showBoneNames": False,
+    "emptyValue": None,
+    "defaultLanguage": "de",
+    "flare.icon.svg.embedding.path": "/static/svgs",
+    "flare.icon.fallback.error": "icon-error",
+    "flare.icon.cache": {},
+    "flare.language.current": "de",
+    "expressionEvaluator": ShitEval({
+        "translate": translate
+    })
+}
+
+# Assign SafeEval as htmlExpressionEvaluator
+core.htmlExpressionEvaluator = conf["expressionEvaluator"]
+
+# Merge view_conf into main config
+from flare.views import conf as view_conf
+
+updateConf(view_conf)
