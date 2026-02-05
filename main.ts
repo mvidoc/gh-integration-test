f2022e2522dfaa508ef7de8cf603925e476c8fcc
mvidoc <mikolaj@vidocsecurity.com>
2026-02-05 15:48:36 +0100

    xd

--- main.ts
+++ main.ts
@@ -11,4 +11,4 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => console.log(`listening on ${PORT}`));
 
-console.log("wrxwwwd");
+console.log("xd");
