fe1c4a0bcdd0c3d3582b2318c60f59b470a814c6
mvidoc <mikolaj@vidocsecurity.com>
2026-02-13 18:35:35 +0100

    xd

--- main.ts
+++ main.ts
@@ -24,7 +24,7 @@ app.get("/health", (_req: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Serdver rdunning on port ${PORT}`);
+  console.log(`Serdver rdunnwing on port ${PORT}`);
 });
 
 export default app;
