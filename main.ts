2c8efc272c7c33cf44148ee81aa2ddf533b0edae
mvidoc <mikolaj@vidocsecurity.com>
2026-02-13 18:18:44 +0100

    xd

--- main.ts
+++ main.ts
@@ -24,7 +24,7 @@ app.get("/health", (_req: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server rdunning on port ${PORT}`);
 });
 
 export default app;
