d2b1a537ef2358942337aaab34db37584d2e5cf3
mvidoc <mikolaj@vidocsecurity.com>
2026-02-16 17:40:22 +0100

    xd

--- main.ts
+++ main.ts
@@ -24,7 +24,7 @@ app.get("/health", (_req: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Sexdrdver rdunnwing on port ${PORT}`);
+  console.log(`Sexdxrdver rdunnwing on port ${PORT}`);
 });
 
 export default app;
