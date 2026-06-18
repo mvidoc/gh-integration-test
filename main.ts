37fb0bcdf114e4fd4f1cbd19b0cb88f307318a12
mvidoc <mikolaj@vidocsecurity.com>
2026-06-18 13:15:31 +0200

    x

--- main.ts
+++ main.ts
@@ -24,7 +24,7 @@ app.get("/health", (_req: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`xwwdd rwarwa on port ${PORT}`);
+  console.log(`xwwdd rwwarwa on port ${PORT}`);
 });
 
 export default app;
